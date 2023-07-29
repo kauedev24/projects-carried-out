fb6151cfc42ad039821e0345685a0d6ca7dcffdb
Kaue Brito <knascimento353@gmail.com>
2023-07-29 01:04:19 -0300

    art.py

---
+++ GuessNumber/art.py
@@ -0,0 +1,7 @@
+LOGO = """
+  ooooooo8 ooooo  oooo ooooooooooo  oooooooo8  oooooooo8            o           oooo   oooo ooooo  oooo oooo     oooo oooooooooo ooooooooooo oooooooooo  
+o888    88  888    88   888    88  888        888                  888           8888o  88   888    88   8888o   888   888    888 888    88   888    888 
+888    oooo 888    88   888ooo8     888oooooo  888oooooo          8  88          88 888o88   888    88   88 888o8 88   888oooo88  888ooo8     888oooo88  
+888o    88  888    88   888    oo          888        888        8oooo88         88   8888   888    88   88  888  88   888    888 888    oo   888  88o   
+ 888ooo888   888oo88   o888ooo8888 o88oooo888 o88oooo888       o88o  o888o      o88o    88    888oo88   o88o  8  o88o o888ooo888 o888ooo8888 o888o  88o8 
+"""
